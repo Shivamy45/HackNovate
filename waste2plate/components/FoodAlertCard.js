40449c2eb7b32a79fa2d89afe4da1e6864b0d6c4
Shivam <shivamy476@gmail.com>
2025-04-04 12:35:05 +0530

    file structure

---
+++ waste2plate/components/FoodAlertCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FoodAlertCard = () => {
+  return (
+    <div>
+      Food alert card
+    </div>
+  )
+}
+
+export default FoodAlertCard
---
+++ waste2plate/components/FoodAlertForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FoodAlertForm = () => {
+  return (
+    <div>
+      Food alert form
+    </div>
+  )
+}
+
+export default FoodAlertForm
